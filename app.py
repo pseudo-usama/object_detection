325f42e0c25c66638708661611c93c70a233d61f
PSEUDO <Pseudo.Developers@gmail.ccom>
2020-11-28 21:44:31 +0500

    routes created for ONLY SUBMIT & SEARCH & a little bit frontend updated

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, render_template
+from os import rename as move_file
+from os.path import splitext as splitFileName
+from uuid import uuid1
+from flask import Flask, request, render_template, redirect
 from config import *
 from detector import detect_objs
 from DB import insert as insert_toDB
@@ -9,18 +12,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data={'error': 'no_data_and_error'})
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    img = request.files['img']
-    img.save(f'{UPLOADED_IMGS_DIR}/{img.filename}')
-    objs = detect_objs(img.filename)
+    imgName = save_file()
+    objs = detect_objs(imgName)
     if objs is not None:
+        move_file(UPLOADED_IMGS_DIR+imgName, INDEXED_IMGS_DIR+imgName)
         insert_toDB(objs)
 
-    return render_template('index.html')
+    return redirect('/')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    imgName = save_file()
+    objs = detect_objs(imgName)
+    if objs is not None:
+        # Now search in database
+        # Move submited img results in DB
+        # And Move img to INDEXED_IMGS_DIR
+        # return results
+        # return no objects detected msj
+        pass
+
+
+def save_file():
+    img = request.files['img']
+    imgName = str(uuid1())+splitFileName(img.filename)[1]
+    img.save(UPLOADED_IMGS_DIR+imgName)
+
+    return imgName
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 UPLOADED_IMGS_DIR = 'images/'
+INDEXED_IMGS_DIR = 'indexed-images/'
