a05a3d5a599fc275c2c02bcc8fabb15f37910199
PSEUDO <Pseudo.Developers@gmail.ccom>
2021-05-22 22:36:47 +0500

    validation added for select-bounding-boxes-types template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from os import rename as move_file
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect
 import json
 
 # Local imports
@@ -7,26 +7,22 @@ from config import *
 from detector import detect_objs, document_detertor
 from DB import insert as insert_toDB
 from DB.search import search as search_inDB
-from validation import validate
+from validation import validate_new_img_req, validate_bounding_boxes_selector_req
 
 
 app = Flask(__name__)
 
 
+# Index page
 @app.route('/')
 def index():
     return render_template('index.html', template='default')
+    # return render_template('index.html', template='document-bounding-boxes-selector', img_name='6dbf3c8d-baf4-11eb-883f-6c2b5950783c.jpg', img_path=INDEXED_IMGS_DIR+'6dbf3c8d-baf4-11eb-883f-6c2b5950783c.jpg', bounding_boxes=json.dumps([{'topLeft': (1076, 424), 'dimensions': (136, 84), 'text': 'This', 'distance': (912, 187)}, {'topLeft': (1260, 432), 'dimensions': (43, 63), 'text': 'is', 'distance': (1096, 195)}, {'topLeft': (1338, 428), 'dimensions': (325, 67), 'text': 'first', 'distance': (1174, 191)}, {'topLeft': (1527, 424), 'dimensions': (140, 84), 'text': 'text', 'distance': (1363, 187)}, {'topLeft': (55, 1128), 'dimensions': (124, 85), 'text': 'And', 'distance': (-109, 891)}, {'topLeft': (221, 1128), 'dimensions': (225, 85), 'text': "that's", 'distance': (57, 891)}, {'topLeft': (483, 1128), 'dimensions': (211, 85), 'text': 'second', 'distance': (319, 891)}, {'topLeft': (752, 1133), 'dimensions': (157, 66), 'text': 'line', 'distance': (588, 896)}, {'topLeft': (1754, 1207), 'dimensions': (67, 52), 'text': '(7', 'distance': (1590, 970)}, {'topLeft': (1127, 1287), 'dimensions': (126, 58), 'text': '——', 'distance': (963, 1050)}, {'topLeft': (1358, 1170), 'dimensions': (304, 180), 'text': 'oe', 'distance': (1194, 933)}, {'topLeft': (1694, 1214), 'dimensions': (103, 87), 'text': 'a!', 'distance': (1530, 977)}, {'topLeft': (1798, 1213), 'dimensions': (139, 101), 'text': 'me', 'distance': (1634, 976)}]))
 
 
-@app.route('/document-bounding-boxes-selector', methods=['POST'])
-def save_template():
-    print(request.form['img-name'])
-
-    return redirect('/')
-
-
+# When user only submits a image/document
 @app.route('/submit', methods=['POST'])
-@validate
+@validate_new_img_req
 def submit(img_name, req_type):
     if req_type == 'image':
         objs = detect_objs(img_name)
@@ -46,8 +42,9 @@ def submit(img_name, req_type):
         return render_template('index.html', template='document-bounding-boxes-selector', img_name=img_name, img_path=INDEXED_IMGS_DIR+img_name, bounding_boxes=json.dumps(bbs))
 
 
+# When user search for image/document
 @app.route('/search', methods=['POST'])
-@validate
+@validate_new_img_req
 def search(img_name, img_type):
     objs = detect_objs(img_name)
 
@@ -63,6 +60,17 @@ def search(img_name, img_type):
 
     imgs = [INDEXED_IMGS_DIR+img for img in imgs]
     return render_template('index.html', template='gallary', imgs=imgs)
+
+
+# When user enter document template
+# This route used only after submitting of document
+@app.route('/document-bounding-boxes-selector', methods=['POST'])
+@validate_bounding_boxes_selector_req
+def save_template(img_name, bounding_boxes_data):
+    print(img_name)
+    print(bounding_boxes_data)
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
--- validation.py
+++ validation.py
@@ -1,5 +1,6 @@
 from os.path import splitext as splitFileName
 from functools import wraps
+from subprocess import call
 from flask import request
 from flask_api.status import HTTP_400_BAD_REQUEST
 from uuid import uuid1
@@ -8,23 +9,23 @@ from uuid import uuid1
 from config import *
 
 
-def validate(callback):
+def validate_new_img_req(callback):
     @wraps(callback)
-    def chk_img_type_and_save(*args, **kwargs):
+    def validate_and_save_img(*args, **kwargs):
         if 'img' not in request.files:
-            return """<h1>Bad request 400</h1><p>Add an image</p>""", HTTP_400_BAD_REQUEST
+            return BAD_REQUEST('Add an image'), HTTP_400_BAD_REQUEST
         elif 'img-type' not in request.form:
-            return """<h1>Bad request 400</h1><p>Select some image type</p>""", HTTP_400_BAD_REQUEST
+            return BAD_REQUEST('Select some image type'), HTTP_400_BAD_REQUEST
 
         req_type = request.form['img-type']
         img_file = request.files['img']
         if req_type != 'image' and req_type != 'document':
-            return """<h1>Bad request 400</h1><p>Image type can only be 'image' or 'document'</p>""", HTTP_400_BAD_REQUEST
+            return BAD_REQUEST("Image type can only be 'image' or 'document'"), HTTP_400_BAD_REQUEST
 
         img_name = save_file(img_file)
 
         return callback(img_name, req_type, *args, **kwargs)
-    return chk_img_type_and_save
+    return validate_and_save_img
 
 
 def save_file(img):
@@ -32,3 +33,23 @@ def save_file(img):
     img.save(UPLOADED_IMGS_DIR+img_name)
 
     return img_name
+
+
+def validate_bounding_boxes_selector_req(callback):
+    wraps(callback)
+
+    def validate_bounding_boxes_form(*args, **kwargs):
+        if 'img-name' not in request.form:
+            return BAD_REQUEST('Image name not found.')
+        elif 'bounding-boxes-data' not in request.form:
+            return BAD_REQUEST('No bounding boxes data found.')
+
+        img_name = request.form['img-name']
+        bounding_boxes_data = request.form['bounding-boxes-data']
+
+        return callback(img_name, bounding_boxes_data, *args, **kwargs)
+    return validate_bounding_boxes_form
+
+
+def BAD_REQUEST(msj=''):
+    return f'<h1>Bad request</h1><p>{msj}</p>'
