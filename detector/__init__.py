f2f86c5dedc837e972430bbaffb44a5ee1410fa2
Usama (PSEUDO) <pseudo.usama@gmail.com>
2021-06-26 12:27:33 +0500

    a small bug from previous commit

--- detector/__init__.py
+++ detector/__init__.py
@@ -68,7 +68,7 @@ def index_for_DB(objects, imgName, add_deviation=False):
     
     for i, obj in enumerate(objects):
         if i == 0:
-            data['1.1'] = imgName
+            data[f'1.{no_of_objs}'] = imgName
             continue
 
         nthAreaRatioObj = ceil(obj['areaRatio']/OBJ_AREA_RATIO_RANGE)
