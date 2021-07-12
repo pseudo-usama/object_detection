3db7bc522a90a534f893d25fd8f2bfc5846cc76f
Usama (PSEUDO) <pseudo.usama@gmail.com>
2021-07-12 10:19:16 +0500

    data structure file added for 1 & 2 graphical objs

---
+++ DB/Data structure/1-graphical-obj.js
@@ -0,0 +1,13 @@
+// The code to generate this data structure is here: indexing\one_graphical_obj\index.py
+
+a = {
+    1: {    // Width & height ratio of graphical obj
+        1: {    // No. of SBBs
+            1: {    // No. of DBBs
+                sbb: [{ val: 'some text', pos: [1, 1], size: [1, 1] }],
+                dbb: [{ regex: '' }],
+                img: ''
+            }
+        }
+    }
+}
---
+++ DB/Data structure/2-graphical-objs.js
@@ -0,0 +1,32 @@
+// The code to generate this data structure is here: indexing\two_graphical_objs\index.py
+
+a = {
+    10: {   // Angles obj
+        10: {   // Ratio of corners of objs (explanation of calculation is given below)
+            1: {    // No. of SBBs
+                1: {    // No. of DBBs
+                    sbb: [{ val: 'some text', pos: [1, 1], size: [1, 1] }],
+                    dbb: [{ regex: '' }],
+                    img: 'img.jpg'
+                }
+            }
+        }
+    }
+
+
+/*
+    Calculation of corners ratio of objects
+
+    Object 1                            Object 2
+    c1 ---------- c2                    c5 ---------- c6
+    |             |                     |             |
+    |             |                     |             |
+    |             |                     |             |
+    |             |                     |             |
+    c3 ---------- c4                    c7 ---------- c8
+
+    The ratio is calculated using this formula:
+        ratio1 = distanceOf(c1, c5) / distanceOf(c4, c8)
+        ratio2 = distanceOf(c2, c6) / distanceOf(c3, c7)
+        finalRatio = ratio1 / ratio2
+*/
